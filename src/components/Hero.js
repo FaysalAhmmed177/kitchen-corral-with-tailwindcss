dee92c39ed2c80cb8074f107d573d7955606be64
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-06-09 16:40:12 +0600

    kitchen corral navbar done

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className="bg-white h-screen flex flex-col justify-center items-center">
+            <h1 className="lg:text-9xl md-text-7xl sm:text-5xl text-3xl font-black mb-14">
+                Kitchen Corral
+            </h1>
+        </div>
+    );
+};
+
+export default Hero;
